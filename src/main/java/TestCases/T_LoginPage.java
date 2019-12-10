c6e68f6d7029a79a3456b063c4b31cfc20b0d586
Ashokpandian <ashokpandian56@hotmail.com>
2019-12-10 15:49:09 +0530

    ignored log files

--- src/main/java/TestCases/T_LoginPage.java
+++ src/main/java/TestCases/T_LoginPage.java
@@ -2,16 +2,19 @@ package TestCases;
 
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Optional;
 
 import GuruPages.LoginPage;
 
 public class T_LoginPage extends TestBase{
 	
 	@Test
-	public static void loginTest() {
+	@Parameters({"username","password"})
+	public static void loginTest(@Optional("as198184@gmail.com") String username, @Optional("Guru@2019") String password) {
 		PageFactory.initElements(driver, GuruPages.LoginPage.class);
-		LoginPage.txtUserName.sendKeys("as198184@gmail.com");
-		LoginPage.txtPassword.sendKeys("Guru@2019");
+		LoginPage.txtUserName.sendKeys(username);
+		LoginPage.txtPassword.sendKeys(password);
 		LoginPage.btnLogin.click();	
 		}
 }
