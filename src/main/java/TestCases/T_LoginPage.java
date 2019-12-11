5fbfe5a44da11c9d9e7e1cbca8abeb1282eeec9f
Ashokpandian <ashokpandian56@hotmail.com>
2019-12-11 14:45:34 +0530

    Fourth Commit

--- src/main/java/TestCases/T_LoginPage.java
+++ src/main/java/TestCases/T_LoginPage.java
@@ -2,6 +2,9 @@ package TestCases;
 
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Optional;
 
@@ -12,9 +15,13 @@ public class T_LoginPage extends TestBase{
 	@Test
 	@Parameters({"username","password"})
 	public static void loginTest(@Optional("as198184@gmail.com") String username, @Optional("Guru@2019") String password) {
+		Extentlog = ExtentReport.createTest(T_LoginPage.class.getName());
 		PageFactory.initElements(driver, GuruPages.LoginPage.class);
 		LoginPage.txtUserName.sendKeys(username);
+		Extentlog.log(Status.PASS,"User Name : '"+username+"' is Entered");
 		LoginPage.txtPassword.sendKeys(password);
+		Extentlog.log(Status.PASS,"User Name : '"+password+"' is Entered");
 		LoginPage.btnLogin.click();	
+		Extentlog.log(Status.PASS,"Login Button is Clicked");
 		}
 }
--- src/main/java/TestCases/TestBase.java
+++ src/main/java/TestCases/TestBase.java
@@ -13,10 +13,24 @@ import org.testng.TestNG;
 import org.testng.annotations.AfterTest;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 
 public class TestBase {
 	
 	public static WebDriver driver;
+	public static Logger logger = Logger.getLogger(TestBase.class);
+	public static ExtentHtmlReporter htmlreporter;
+	public static ExtentReports ExtentReport;
+	public static ExtentTest Extentlog;
+	
 	
 	@BeforeTest
 	public static void initBrowserDriver() throws IOException {	
@@ -29,18 +43,31 @@ public class TestBase {
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		PropertyConfigurator.configure("log4j.properties");
+		logger.info("'"+getPropertyValue("browser")+"' Browser is launched Successfully");
+		ExtentReport = new ExtentReports();
+		htmlreporter = new ExtentHtmlReporter("ExtentReport.html");
+		ExtentReport.attachReporter(htmlreporter);
+		htmlreporter.config().setDocumentTitle(getPropertyValue("ExtentDocumentTitle"));
+		htmlreporter.config().setReportName(getPropertyValue("ExtentReportTitle"));
+		htmlreporter.config().setTheme(Theme.STANDARD);
+		ExtentReport.setSystemInfo("Host Name", InetAddress.getLocalHost().getHostName());
+		ExtentReport.setSystemInfo("Host 2", System.getenv("HOSTNAME"));
 //		driver.get("http://demo.guru99.com/insurance/v1/index.php");		
 	}
 	
 	@BeforeTest(dependsOnMethods="initBrowserDriver")
 	public static void GuruLogin() throws IOException {
-		driver.get(getPropertyValue("apploginurl"));		
+		driver.get(getPropertyValue("apploginurl"));
+		logger.info("URL '"+getPropertyValue("apploginurl")+"' is launched Successfully");
 	}
 	
 	
 	@AfterTest
-	public static void KillBrowser() {
+	public static void KillBrowser() throws IOException {
 		driver.quit();
+		logger.info("'"+getPropertyValue("apploginurl")+"' Browser Closed");
+		
 	}
 	
 	
@@ -54,16 +81,12 @@ public class TestBase {
 	
 	public static void main(String args[]) throws IOException {
 		TestNG testng = new TestNG();
-						
 		// Create a list of String 
-		List<String> suitefiles=new ArrayList<String>();
-		 
+		List<String> suitefiles=new ArrayList<String>(); 
 		// Add xml file which you have to execute
 		suitefiles.add("testng.xml");
-		 
 		// now set xml file for execution
 		testng.setTestSuites(suitefiles);
-
 		testng.run();
 	}
 	
